0cd50a33b859b39096cb78bbfe2faf1622af6afc
Vandinelson Rocha <vandInforma.tic@gmail.com>
2022-06-10 09:14:20 -0100

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@
 let somaMussarela = 0;
 let somaMista = 0;
 let somaCalabresa = 0;
-let vendaMussarela = 0.0;
-let vendaMista = 0.0;
-let vendaCalabresa = 0.0;
+let vendaMussarela = 0.00;
+let vendaMista = 0.00;
+let vendaCalabresa = 0.00;
 
 //Variáveis de manipulação do DOM para os botôes:
-window.onload=function()  {
+window.onload = function()  {
   let btnVendas = document.getElementById("vendas");
   let btnEstorno = document.getElementById("estorno");
 
@@ -24,22 +24,20 @@ function venda(e) {
   //Se o usuário escolher o sabor mussarela:
   if (document.getElementById("mussarela").checked) {
     somaMussarela = somaMussarela + 1;
-    vendaMussarela = vendaMussarela + 25.0;
+    vendaMussarela = vendaMussarela + 25.00;
     //Colocando os valores na tabela:
     document.getElementById("resultadoMussarela").innerHTML = somaMussarela;
-    document.getElementById("vendasMussarela").innerHTML =
-      vendaMussarela.toFixed(2);
+    document.getElementById("vendasMussarela").innerHTML = vendaMussarela.toFixed(2);
   } else if (document.getElementById("mista").checked) {
     somaMista += 1;
-    vendaMista += 30.0;
+    vendaMista += 30.00;
     document.getElementById("resultadoMista").innerHTML = somaMista;
     document.getElementById("vendasMista").innerHTML = vendaMista.toFixed(2);
   } else if (document.getElementById("calabresa").checked) {
     somaCalabresa += 1;
-    vendaCalabresa += 35.0;
+    vendaCalabresa += 35.00;
     document.getElementById("resultadoCalabresa").innerHTML = somaCalabresa;
-    document.getElementById("vendasCalabresa").innerHTML =
-      vendaCalabresa.toFixed(2);
+    document.getElementById("vendasCalabresa").innerHTML = vendaCalabresa.toFixed(2);
   }
 }
 
@@ -47,33 +45,31 @@ function estorno(e) {
   //Se o usuário escolher o sabor mussarela:
   if (document.getElementById("mussarela").checked) {
     somaMussarela = somaMussarela - 1;
-    vendaMussarela = vendaMussarela - 25.0;
+    vendaMussarela = vendaMussarela - 25.00;
     //Sendo que, logicamente, os valores de venda e estorno não podem ser negativos:
     if (somaMussarela < 0) {
       somaMussarela = 0;
-      vendaMussarela = 0.0;
+      vendaMussarela = 0.00;
     }
     document.getElementById("resultadoMussarela").innerHTML = somaMussarela;
-    document.getElementById("vendasMussarela").innerHTML =
-      vendaMussarela.toFixed(2);
+    document.getElementById("vendasMussarela").innerHTML = vendaMussarela.toFixed(2);
   } else if (document.getElementById("mista").checked) {
     somaMista -= 1;
-    vendaMista -= 30.0;
+    vendaMista -= 30.00;
     if (somaMista < 0) {
       somaMista = 0;
-      vendaMista = 0.0;
+      vendaMista = 0.00;
     }
     document.getElementById("resultadoMista").innerHTML = somaMista;
     document.getElementById("vendasMista").innerHTML = vendaMista.toFixed(2);
   } else if (document.getElementById("calabresa").checked) {
     somaCalabresa -= 1;
-    vendaCalabresa -= 35.0;
+    vendaCalabresa -= 35.00;
     if (somaCalabresa < 0) {
       somaCalabresa = 0;
-      vendaCalabresa = 0.0;
+      vendaCalabresa = 0.00;
     }
     document.getElementById("resultadoCalabresa").innerHTML = somaCalabresa;
-    document.getElementById("vendasCalabresa").innerHTML =
-      vendaCalabresa.toFixed(2);
+    document.getElementById("vendasCalabresa").innerHTML = vendaCalabresa.toFixed(2);
   }
 }
